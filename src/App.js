211c04f51894ca6783c34964ad269c06ccfbde4b
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-17 04:59:04 +0530

    added input name in home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Home from './Components/Home';
 import './App.css';
 
 class App extends Component {
+
+  // constructor() {
+
+  // }
+
+  // componentDidMount() {
+
+  // }
+
   render() {
     return(
       <Home />
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 class Home extends Component {
 
+    submit = () => {
+        const name = document.getElementById("name").value;
+        console.log(name);
+        document.getElementById("id").innerHTML = name;
+    }
+
     render() {
         return(
             <div>
-                Home
+                <input type = "text" placeholder = "Enter You Name" id = "name" />
+                <br />
+                <button onClick = { this.submit } > Submit </button>
+
+                <p id = "id" ></p>
             </div>
         );
     }
