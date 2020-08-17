20c5226337f9d15f757bb0cea31d5d5b7b2bc550
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-17 05:52:27 +0530

    printed questions and options

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react';
 
 class Question extends Component {
+
+    options = (results, index) => {
+        let options = [];
+
+        options.push( 
+            <div key = { results[index].correct_answer } >
+                <input type = "radio" name = "options" value = { results[index].correct_answer } /> 
+                    { results[index].correct_answer }
+            </div>
+        );
+
+        for(let i = 0; i < results[index].incorrect_answers.length; i++) {
+            options.push( 
+                <div key = { results[index].incorrect_answers[i] } >
+                    <input type = "radio" name = "options" value = { results[index].incorrect_answers[i] } /> 
+                        { results[index].incorrect_answers[i] }
+                </div>
+            );
+        }
+
+        return options;
+    }
+
     render() {
+        const index = this.props.state.index;
+        const results = this.props.state.data.results;
+
         return(
-            <div>Question</div>
+            <div>
+                Question { index+1 } { results[index].question }
+                <form>
+                    { this.options(results, index) }
+                </form>
+            </div>
         );
     }
 }
