a151d5b6814021f0af867d57354ad54e81789f05
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-17 08:17:40 +0530

    added result

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -2,6 +2,44 @@ import React, { Component } from 'react';
 
 class Result extends Component {
 
+    displayAnswers = () => {
+        const answers = this.props.state.answers;
+        const results = this.props.state.data.results;
+
+        let result = [];
+
+        for(let i = 0; i < results.length; i++) {
+            if(answers[i].toString() === results[i].correct_answer.toString()) {
+                result.push(
+                    <div key = { answers[i] } >
+                        { i + 1 } { answers[i] } Correct
+                    </div>
+                );
+            } else if(answers[i].toString() === "") {
+                result.push(
+                    <div key = { answers[i] } >
+                        { i + 1 } { answers[i] } Unattempted
+                    </div>
+                );
+            } else {
+                result.push(
+                    <div key = { answers[i] } >
+                        { i + 1 } { answers[i] } Incorrect
+                    </div>
+                );
+            }
+        }
+
+        return result;
+    }
+
+    render() {
+        return(
+            <div>
+                { this.props.state.name } your score: { this.props.state.score }
+            </div>
+        );
+    }
 }
 
 export default Result;
