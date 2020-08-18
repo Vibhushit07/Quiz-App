ce272684e8108ae63d18a2cca0a3bcf37505e3dd
Vibhushit07 <vibhushitsinghal80@gmail.com>
2020-08-19 00:48:14 +0530

    displayed answers

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -11,19 +11,19 @@ class Result extends Component {
         for(let i = 0; i < results.length; i++) {
             if(answers[i].toString() === results[i].correct_answer.toString()) {
                 result.push(
-                    <div key = { answers[i] } >
+                    <div key = { i + 1 } >
                         { i + 1 } { answers[i] } Correct
                     </div>
                 );
             } else if(answers[i].toString() === "") {
                 result.push(
-                    <div key = { answers[i] } >
+                    <div key = { i + 1 } >
                         { i + 1 } { answers[i] } Unattempted
                     </div>
                 );
             } else {
                 result.push(
-                    <div key = { answers[i] } >
+                    <div key = { i + 1 } >
                         { i + 1 } { answers[i] } Incorrect
                     </div>
                 );
@@ -37,6 +37,9 @@ class Result extends Component {
         return(
             <div>
                 { this.props.state.name } your score: { this.props.state.score }
+                <br />
+                <br />
+                { this.displayAnswers() }
             </div>
         );
     }
